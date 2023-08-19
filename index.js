7092eda7371f7be7a86b5f60980e68da5559cbda
Paddy-Nimrod <n.ambetsa21@gmail.com>
2023-08-19 13:55:53 +0300

    added server file

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express = require("express");
 const bodyparser = require("body-parser");
 
 const app = express();
+const port = process.env.PORT || 3001;
 
-app.use();
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use("/", (req, res) => {
+  res.send({ message: "okay" });
+});
+
+app.listen(port, () => {
+  console.log(`app running on port: ${port}`);
+});
